3267a4eba60c49e4eb2e2d8041b139f04a7cceaf
Davod3 <davidpereira.pereira4@gmail.com>
2022-11-10 16:13:11 +0000

    Working actions

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ class JogoBT_27(Game):
     board = {"a1" : 'W', "b1" : 'W', "c1" : 'W', "d1" : 'W', "e1" : 'W', "f1" : 'W', "g1" : 'W', "h1" : 'W',
                 "a2" : 'W', "b2" : 'W', "c2" : 'W', "d2" : 'W', "e2" : 'W', "f2" : 'W', "g2" : 'W', "h2" : 'W',
                 "a7" : 'B', "b7" : 'B', "c7" : 'B', "d7" : 'B', "e7" : 'B', "f7" : 'B', "g7" : 'B', "h7" : 'B',
-                "a8" : 'B', "b8" : 'B', "c8" : 'B', "d8" : 'B', "e8" : 'B', "f8" : 'B', "g8" : 'B', "h8" : 'B',};
+                "a8" : 'B', "b8" : 'B', "c8" : 'B', "d8" : 'B', "e8" : 'B', "f8" : 'B', "g8" : 'B', "h8" : 'B',}
     moves = ['a2-a3', 'a2-b3', 'b2-a3', 'b2-b3', 'b2-c3', 'c2-b3', 'c2-c3', 'c2-d3',
                 'd2-c3', 'd2-d3', 'd2-e3', 'e2-d3', 'e2-e3', 'e2-f3', 'f2-e3', 'f2-f3',
                 'f2-g3', 'g2-f3', 'g2-g3', 'g2-h3', 'h2-g3','h2-h3']
@@ -22,7 +22,41 @@ class JogoBT_27(Game):
         self.initial = initial
 
     def actions(self, state):
-        return
+        player = self.to_move(state)
+        actions = list()
+
+        for pos in state.board.keys():
+            if state.board.get(pos) == player:
+                #Position occupied by piece Player
+                    col = ord(pos[0])
+                    row = int(pos[1]) + 1
+                    col_limit = 97+8
+                    
+                    #Move forward
+                    if row <= 8:
+                        #Move fits inside board
+                        #Check if there is a player piece there
+                        new_pos = chr(col) + str(row)
+                        if new_pos not in state.board.keys():
+                            actions.append(pos + "-" + new_pos)
+
+                    #Move right
+                    if row <= 8 and col + 1 < col_limit:
+                        #Move fits inside board
+                        #Check if there is a player piece there
+                        new_pos = chr(col+1) + str(row)
+                        if new_pos not in state.board.keys():
+                            actions.append(pos + "-" + new_pos)
+
+                    #Move left
+                    if row <= 8 and col - 1 >= 97:
+                        #Move fits inside board
+                        #Check if there is a player piece there
+                        new_pos = chr(col-1) + str(row)
+                        if new_pos not in state.board.keys():
+                            actions.append(pos + "-" + new_pos)
+                    
+        return sorted(actions)
     
     def result(self, state, move):
         return
@@ -64,3 +98,6 @@ class JogoBT_27(Game):
             
 bt = JogoBT_27()
 bt.display(bt.initial)
+print("------")
+s1 = bt.initial
+print(bt.actions(s1))
