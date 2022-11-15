15298c03e116b1fc8d1a7329389ae25639fc03bd
Miguel Cut <miguelcut44@gmail.com>
2022-11-15 11:44:09 +0000

    belarmino

--- jogadorBT.py
+++ jogadorBT.py
@@ -1,12 +1,30 @@
+from jogoBT import*
 from jogar import*
 from random import*
 
 class jogadorBT_27(JogadorAlfaBeta):
     pass
 
-def func_aval_27(self, state):
+def func_aval_27(self, state, player):
     return 1
-    
-def func_aval_Belarmino(self,state):
-    pass    
-        
+
+def count_pieces(self, state, line, player):
+    count = 0
+    if player == 1:
+        for pos in state.board.keys():
+            if str(line) in pos and state.board.get(pos) == "W":
+                count += 1
+    else:
+        for pos in state.board.keys():
+            if str(line) in pos and state.board.get(pos) == "B":
+                count += 1
+    return count
+
+def func_aval_Belarmino(self,state,player):
+    result = 0
+    for x in range[1,8]:
+        print(x) 
+        result += pow(x,x) * count_pieces(state, x, player)
+    return result
+
+
